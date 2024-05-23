28f7cec28555a9356601383d2899637bf5d6f9bf
alessandroibes <alessandro.ibes@gmail.com>
2024-05-23 19:01:00 -0300

    Iniciando com React: Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { Post } from './Post';
 export function App() {
   return (
     <>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Alessandro Oliveira"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima dolore quo iure soluta facilis eveniet qui nihil necessitatibus velit error fuga quae, voluptatem, quisquam quibusdam ipsum exercitationem sequi, nesciunt temporibus."
+      />
+      <Post author="Outro Autor" content="Um novo post melhor que o anterior" />
     </>
   );
 }
