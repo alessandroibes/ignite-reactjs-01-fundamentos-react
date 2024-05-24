061d6ab16cba7dc9ba856a53f91939d0a3374b72
alessandroibes <alessandro.ibes@gmail.com>
2024-05-24 16:40:02 -0300

    Estrutura da aplicação: Estilizações do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,22 +18,22 @@ export function Post() {
         <time title="24 de Maio às 15:16h" dateTime="2024-05-24 15:16:00">
           Publicado há 1h
         </time>
-
-        <div className={styles.content}>
-          <p>Fala galeraa 👋</p>
-          <p>
-            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
-            🚀
-          </p>
-          <p>
-            👉<a href="">jane.design/doctorcare</a>
-          </p>
-          <p>
-            <a href="">#novoprojeto #nlw #rocketseat</a>
-          </p>
-        </div>
       </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="">👉 jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
     </article>
   );
 }
