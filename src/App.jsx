48e645e2f4d3020d07709a2afff53c810daff58d
alessandroibes <alessandro.ibes@gmail.com>
2024-05-24 15:03:07 -0300

    Estrutura da aplicação: Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-        author="Alessandro Oliveira"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima dolore quo iure soluta facilis eveniet qui nihil necessitatibus velit error fuga quae, voluptatem, quisquam quibusdam ipsum exercitationem sequi, nesciunt temporibus."
-      />
-      <Post author="Outro Autor" content="Um novo post melhor que o anterior" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Alessandro Oliveira"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima dolore quo iure soluta facilis eveniet qui nihil necessitatibus velit error fuga quae, voluptatem, quisquam quibusdam ipsum exercitationem sequi, nesciunt temporibus."
+          />
+          <Post
+            author="Outro Autor"
+            content="Um novo post melhor que o anterior"
+          />
+        </main>
+      </div>
     </div>
   );
 }
