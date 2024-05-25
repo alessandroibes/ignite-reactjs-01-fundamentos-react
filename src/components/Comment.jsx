e7240c816ced0dcf22d7a9b8c487dbfd0fcb628e
alessandroibes <alessandro.ibes@gmail.com>
2024-05-25 12:12:07 -0300

    Estrutura da aplicação: Componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import styles from './Comment.module.css';
+
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/alessandroibes.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Alessandro Oliveira</strong>
+              <time title="25 de Maio às 08:13h" dateTime="2024-05-25 08:13:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
