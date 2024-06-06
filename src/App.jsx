9e18e48adad9c3df7a4cad9e7daa3a3be63bf9c3
alessandroibes <alessandro.ibes@gmail.com>
2024-06-06 15:46:51 -0300

    Os motores do React: Iterando no JSX

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,45 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/alessandroibes.png',
+      name: 'Alessandro Oliveira',
+      role: 'Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/alessandroibes.png',
+      name: 'Alessandro Oliveira',
+      role: 'Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-05-10 20:00:00'),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -14,14 +53,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Alessandro Oliveira"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima dolore quo iure soluta facilis eveniet qui nihil necessitatibus velit error fuga quae, voluptatem, quisquam quibusdam ipsum exercitationem sequi, nesciunt temporibus."
-          />
-          <Post
-            author="Outro Autor"
-            content="Um novo post melhor que o anterior"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
